c4c930783ee7e1007082ffac664153e7bda4f85e
Brian N <0xb3rlin@gmail.com>
2023-03-16 17:47:53 -0500

    setup context

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,20 +1,39 @@
 import React, { createContext, useState } from 'react';
 
-const ProductsContext = createContext();
+export const ProductsContext = createContext();
 
 export const ProductsProvider = ({ children }) => {
-  const [test, setTest] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalCost, setTotalCost] = useState(0);
+
+  const addToCart = (product) => {
+    // update cartItems and totalCost state based on the product being added to the cart
+  };
+
+  const removeFromCart = (productId) => {
+    // update cartItems and totalCost state based on the product being removed from the cart
+  };
 
   return (
     <ProductsContext.Provider
       value={{
-        test,
-        setTest,
+        products,
+        filteredProducts,
+        selectedFilters,
+        currentPage,
+        itemsPerPage,
+        cartItems,
+        totalCost,
+        addToCart,
+        removeFromCart,
       }}
     >
       {children}
     </ProductsContext.Provider>
   );
 };
-
-export default ProductsContext;
