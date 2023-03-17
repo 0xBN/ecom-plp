aa3e548fef3b34da25e99d61ec6ef06a3cc70fdc
Brian N <0xb3rlin@gmail.com>
2023-03-16 22:45:47 -0500

    refactor FilterForm and extracted priceRangeButton

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -1,4 +1,37 @@
+import { useContext } from 'react';
+import { ProductsContext } from '../contexts/ProductsContext';
+import PriceRangeButton from './PriceRangeButton';
+
 const FilterForm = () => {
-  return <div>FilterForm</div>;
+  const { priceRange } = useContext(ProductsContext);
+
+  const priceRanges = [
+    { min: 0, max: 10 },
+    { min: 10, max: 20 },
+    { min: 20, max: 30 },
+    { min: 30, max: 40 },
+    { min: 40, max: 50 },
+    { min: 50, max: 60 },
+    { min: 60, max: 70 },
+    { min: 70, max: 80 },
+    { min: 80, max: 90 },
+    { min: 90, max: 100 },
+  ];
+
+  return (
+    <div>
+      <h2>Filter Form</h2>
+      <div>
+        {priceRanges.map(({ min, max }) => (
+          <PriceRangeButton
+            key={`${min}-${max}`}
+            minPrice={min}
+            maxPrice={max}
+            isActive={min === priceRange.min && max === priceRange.max}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default FilterForm;
--- src/components/tests/FilterForm.test.jsx
+++ src/components/tests/FilterForm.test.jsx
@@ -1,2 +1,29 @@
-// Allow the user to filter the list of products based on their price range.
-// Add additional filters (e.g., by color, size, etc.)
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {
+  ProductsProvider,
+  ProductsContext,
+} from '../../contexts/ProductsContext';
+import FilterForm from '../FilterForm';
+
+describe('FilterForm', () => {
+  it('renders a set of price range buttons', () => {
+    render(
+      <ProductsProvider>
+        <FilterForm />
+      </ProductsProvider>
+    );
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(10);
+    expect(buttons[0]).toHaveTextContent('Below $10');
+    expect(buttons[1]).toHaveTextContent('$10 - $20');
+    expect(buttons[2]).toHaveTextContent('$20 - $30');
+    expect(buttons[3]).toHaveTextContent('$30 - $40');
+    expect(buttons[4]).toHaveTextContent('$40 - $50');
+    expect(buttons[5]).toHaveTextContent('$50 - $60');
+    expect(buttons[6]).toHaveTextContent('$60 - $70');
+    expect(buttons[7]).toHaveTextContent('$70 - $80');
+    expect(buttons[8]).toHaveTextContent('$80 - $90');
+    expect(buttons[9]).toHaveTextContent('$90 - $100');
+  });
+});
