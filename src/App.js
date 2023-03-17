1f7b9751eab5a6653a0304079700a2b720453c91
Brian N <0xb3rlin@gmail.com>
2023-03-16 20:02:10 -0500

    render component temporarily

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import ProductItem from './components/ProductItem';
 const App = () => {
-  return <div>App</div>;
+  const testProduct = {
+    name: 'Name of product',
+    price: 10,
+    image: '',
+    description: 'test description',
+    color: 'red',
+    sizes: ['S', 'M'],
+  };
+
+  return (
+    <div>
+      <ProductItem product={testProduct} />
+    </div>
+  );
 };
 export default App;
